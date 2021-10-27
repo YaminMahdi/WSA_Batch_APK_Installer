1f714e3da401538745b646419b760bc2c78c7556
YK Mahdi <ahmad15-1071@diu.edu.bd>
2021-10-28 00:04:26 +0600

    Installed APKs will not install & ADB Build-in.
    Installed APKs will not install again. ADB installation is now optional.

--- 2.installAllApk[RunWithPowerShell].ps1
+++ 2.installAllApk[RunWithPowerShell].ps1
@@ -1,32 +1,97 @@
-echo "# # # # # # # # # # # # # #"
-echo "# # APK Installer by yk # #"
-echo "# # # # # # # # # # # # # #"
+Write-Host "# # # # # # # # # # # # # # # # # # # # #"
+Write-Host "# # Batch APK Installer for WSA by yk # #"
+Write-Host "# # # # # # # # # # # # # # # # # # # # #"
 #Read-Host
-#<#
-adb devices
-adb disconnect
-adb connect 127.0.0.1:58526
-adb devices
+
+tools.\adb devices
+tools.\adb disconnect
+tools.\adb connect 127.0.0.1:58526
+tools.\adb devices
 
 
 $files = Get-Item *.apk
 $count = 0
+$inCount = 0
 foreach ($apks in $files.Name)
 {
 	if ( $count -ne 0 )
 	{
-		echo ""
+		Write-Host ""
 	}
-	echo "------------------------------------------"
+	Write-Host "------------------------------------------"
 	$count++;
-	echo "Installing Apk No.- $count. `n`t($apks)`n"
-	adb install $apks;
+	Write-Host "Found Apk No.- $count. `nName         : $apks"
+	$yk = tools.\aapt dump badging $apks | Select-String "package: name="
+	$yk = $yk.ToString()
+
+#geting package name
+	$pac=''
+	for($i=15; ; $i++)
+	{
+		if($yk[$i] -eq '''')
+		{
+			break
+		}
+		$pac = $pac.Insert($i-15,$yk[$i]);
+	}
+	Write-Host "Package Name : $pac"
+
+#geting version
+	$in=$yk.IndexOf("versionName='")+13
+	$ver=''
+	for( $i=$in; ; $i++)
+	{
+		if($yk[$i] -eq '''')
+		{
+			break
+		}
+		$ver = $ver.Insert($i-$in,$yk[$i]);
+	}
+	Write-Host "Version      : $ver"
+
+#installing apk
+	$x= tools.\adb shell 'pm list packages'
+	if($x.Contains("package:$pac"))
+	{
+		Write-Host "A version of this APK is already installed.`nTring to update..`nGeting Version.."
+		$inVer= tools.\adb shell dumpsys package $pac | Select-String "versionName"
+		$inVer= $inVer.ToString()
+		
+		$z="    versionName=" + $ver
+		if($inVer.equals($z))
+		{
+			Write-Host "Installed Version: $ver."
+			Write-Host "APK Installation Failed.`nSame vesion is already installed."
+			Write-Host "You can only UPGRADE or DOWNGRADE a APK."
+		}
+		else
+		{
+			Write-Host "Updating APK to letest version."
+			tools.\adb install $apks;
+			$inCount++;
+		}
+	}
+	else
+	{
+		Write-Host "Now installing- $apks."
+		tools.\adb install $apks;
+		$inCount++;
+	}
 }
-echo "`n------------------------------------------"
+Write-Host "`n------------------------------------------"
 
-echo "$count APKS installed from this folder.`n`nOk, tata, by by."
-adb disconnect
-echo "Press any key to continue . . ."
+Write-Host "$count APKS found in this folder. `n$inCount Installed.`n`nOk, tata, by by."
+tools.\adb disconnect
+Write-Host "Press any key to continue . . ."
 [Console]::ReadKey()
-##>
 
+
+
+
+<#
+
+$yk = .\aapt dump badging D:\wsa\apps\flite.apk | Select-String "package: name="
+Write-Host $yk
+Write-Host -match ("com.facebook.lite")
+$x.GetType()
+#>
